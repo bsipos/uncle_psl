9891e12de8b5a6f4f0159b227a857a7ce04d8d23
Botond Sipos <Botond.Sipos@nanoporetech.com>
2017-01-23 09:23:45 +0000

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+from glob import glob
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+requirements = [
+    # TODO: put package requirements here
+]
+
+test_requirements = [
+    # TODO: put package test requirements here
+]
+
+setup(
+    name='uncle_psl',
+    version='0.1.0',
+    description="A BLAT to SAM converter.",
+    long_description=readme,
+    author="Botond Sipos",
+    author_email='Botond.Sipos@nanoporetech.com',
+    url='',
+    packages=[
+        'uncle_psl',
+    ],
+    package_dir={'uncle_psl':
+                 'uncle_psl'},
+    include_package_data=True,
+    install_requires=requirements,
+    zip_safe=False,
+    keywords='uncle_psl',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+    tests_require=test_requirements,
+    scripts=[x for x in glob('scripts/*.py') if x != 'scripts/__init__.py']
+)
